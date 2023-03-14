0f7697d3cb58bfa2fee21a03a583659079f6deb6
Zakariyya Kadri <zakariyyakadri@gmail.com>
2023-03-14 11:12:24 +0000

    add some structure and css for web tech coursework

--- src/index.php
+++ src/index.php
@@ -4,11 +4,38 @@
 <head>
     <meta charset=UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <!-- <link rel="stylesheet" href=""> -->
+    <title>Zakariyya Kadri's Portfolio</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
     <!-- <script src=""> -->
 </head>
 
 <body>
+    <header></header>
+    <nav></nav>
+
+    <main>
+        <section id="aboutme"></section>
+
+        <section id="skillsachievements"></section>
+
+        <section id="educationqualifications"></section>
+
+        <section id="experience"></section>
+
+        <section id="portfolio"></section>
+
+        <section id="contact"></section>
+    </main>
+
+    <aside>
+        <h2>Check out my blog!</h2>
+        <p>My blog is the best way to see what I've been up to</p>
+        <a href="#"><p>Find it here</p></a>
+    </aside>
+
+    <footer></footer>
 </body>
 
 </html>
