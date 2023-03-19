63159ad9ecde5b2edd0e20085d00802a170539c8
Zakariyya Kadri <zakariyyakadri@gmail.com>
2023-03-19 09:59:08 +0000

    Nav should now be unified across all pages, there has to be a better way :pepe_hands:

--- src/viewBlog.php
+++ src/viewBlog.php
@@ -17,14 +17,14 @@
     </header>
     <nav>
         <ul>
-            <li><a href=".">Main</a></li>
-            <li><a href="#">Blog</a></li>
-
-            <!-- TODO: some php stuff to change login to logout depending -->
+            <li><a href="index.php">Main</a></li>
+            <li><a href="viewBlog.php">Blog</a></li>
+            <li><a href="contact.html">Contact me</a></li>
+            <li><a href="education.html">My education</a></li>
             <li><a href="login.html">Login</a></li>
+            <li><a href="addEntry.php">Add blog entry</a></li>
             <li><a href="logout.php">Logout</a></li>
-
-            <li><a href="addEntry.php">Add Entry</a></li>
+            <li><a href="my_works.html">Projects</a></li>
         </ul>
     </nav>
 
