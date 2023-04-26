ae7ac175cd68fd2186fe1be3524ec5b1822b5db1
Zakariyya Kadri <zakariyyakadri@gmail.com>
2023-04-26 18:21:39 +0100

    the  didnt have quotes breaking the js

--- src/viewBlog.php
+++ src/viewBlog.php
@@ -10,13 +10,13 @@
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/common_mobile.css" media="screen and (max-width: 768px)">
     <link rel="stylesheet" href="css/viewBlog.css">
-    <script defer>
-        const USERNAME = <?php 
+    <script>
+        const USERNAME = "<?php 
                         if (is_null($_SESSION))
                             echo ""; 
                         else
                             echo $_SESSION["username"];
-                    ?>;
+                    ?>";
     </script>
     <script src="js/dialog.js" defer></script>
     <script src="js/viewBlog.js" defer></script>
