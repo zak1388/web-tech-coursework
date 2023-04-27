82c65045f8bf5d07d6ac292e5d2cf34b1e63a023
Zakariyya Kadri <zakariyyakadri@gmail.com>
2023-04-27 09:32:31 +0100

    get posts now insertion sorts

--- src/getPosts.php
+++ src/getPosts.php
@@ -1,5 +1,7 @@
 <?php
 
+$posts = [];
+
 try {
     $mysqli = new mysqli("127.0.0.1", "417coursework", "coursework", "courseworkdb");
 
@@ -12,12 +14,43 @@ try {
         die();
     }
 
-    header("Content-Type: application/json; charset=utf-8");
+    $posts = $results->fetch_all(MYSQLI_ASSOC);
 
-    echo json_encode($results->fetch_all(MYSQLI_ASSOC));
 } catch (Exception $exception) {
     echo $exception;
     die();
 }
+
+function getTimestamp($post) {
+    return (new DateTime($post["Date"]))->getTimestamp();
+}
+
+function timestampGreaterThan($post0, $post1) {
+    return getTimestamp($post0) > getTimestamp($post1);
+}
+
+
+// insertion sort posts
+$sorted_posts = [];
+for ($i = 0; $i < count($posts); $i++) {
+    $shift = null;
+    $sorted_posts_count = count($sorted_posts);
+    for ($j = 0; $j < $sorted_posts_count + 1; $j++) {
+        if (!is_null($shift)) {
+            $temp = $sorted_posts[$j];
+            $sorted_posts[$j] = $shift;
+            $shift = $temp;
+        }
+        else if (count($sorted_posts) == $j) {
+            $sorted_posts[$j] = $posts[$i];
+        }else if (timestampGreaterThan($posts[$i], $sorted_posts[$j])) {
+            $shift = $sorted_posts[$j];
+            $sorted_posts[$j] = $posts[$i];
+        }
+    }
+}
+
+header("Content-Type: application/json; charset=utf-8");
+echo json_encode($sorted_posts);
 
 ?>
